59d76914e435e7bb36e80430c5390474cd2c67fe
ispeakfishlang <sandleguiz@gmail.com>
2023-10-12 00:25:06 +0200

    initial commit. Add functions structure

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def set_grid_size(): 
+    
+def place_computer_ships():
+    
+def place_player_ships():
+    
+def play_round():
+    
+def check_if_hit():
+    
+def check_if_sunk():
+    
+def check_if_game_over():
+    
+def restart_game():
+    
+def main():
+    set_grid_size()
+    place_computer_ships()
+    place_player_ships()
+    play_round()
+    check_if_hit()
+    check_if_sunk()
+    check_if_game_over()
+    
