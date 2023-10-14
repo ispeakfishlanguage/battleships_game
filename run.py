1becdfba31dfcbabf6e6fe4fb99d0b8db0735421
ispeakfishlang <sandleguiz@gmail.com>
2023-10-14 19:45:36 +0200

    Add styling with colorama

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import random
-import subprocess
 import pyfiglet
 from colorama import Fore, Back, Style, init
 
@@ -11,7 +10,7 @@ init(autoreset=True)
 text_to_display = "BattleShips"
 
 # Create a FIGlet object with a specific font
-font_name = "3x5"
+font_name = "doh"
 figlet_text = pyfiglet.Figlet()
 
 # Generate ASCII art text using pyfiglet
@@ -29,6 +28,47 @@ SHIP_ICONS = {
     'Destroyer': Fore.YELLOW + 'D'
 }
 EMPTY = '.'
+# Define the instructions with Colorama styling
+INSTRUCTIONS = f"""
+{Fore.BLUE}Instructions:{Style.RESET_ALL}
+
+{Fore.GREEN}Grid Setup:{Style.RESET_ALL} Choose a grid size between
+4x4 and 10x10.
+{Fore.GREEN}Ship Identification:{Style.RESET_ALL} Ships are represented
+by letters:
+    {Fore.CYAN}Battleship (B): Cyan, 4 spaces{Style.RESET_ALL}
+    {Fore.MAGENTA}Cruiser (C): Magenta, 3 spaces{Style.RESET_ALL}
+    {Fore.YELLOW}Destroyer (D): Yellow, 2 spaces{Style.RESET_ALL}
+{Fore.GREEN}Gameplay:{Style.RESET_ALL} Computer and player take turns
+guessing ship positions.
+{Fore.GREEN}Objective:{Style.RESET_ALL} Sink all enemy ships before
+yours are sunk.
+{Fore.GREEN}Endgame:{Style.RESET_ALL} Game ends when all ships of one player
+are sunk and they lose.
+
+{Fore.GREEN}Here is an example of a 10x10 grid setup:{Style.RESET_ALL}
+
+"""
+
+
+def print_example_grid():
+    # Create a 10x10 grid
+    example_grid = 10
+    grid = [['.' for _ in range(example_grid)] for _ in range(example_grid)]
+
+    # Print column labels (A-J)
+    print("    " + " ".join([chr(65 + i) for i in range(example_grid)]))
+
+    # Print the grid with row labels (0-9)
+    for i in range(example_grid):
+        row_label = str(i)
+        # Pad single-digit row labels with a space to align properly
+        if len(row_label) == 1:
+            row_label = " " + row_label
+        print(row_label + " |" + " ".join(grid[i]) + "|")
+
+    # Print a horizontal line to separate the grid from the labels
+    print("    " + "+".join(["-" for _ in range(example_grid)]))
 
 
 def set_grid_size():
@@ -95,6 +135,7 @@ def get_player_ships(player_grid):
     """Ask the user to place their ships and return the player ships."""
     player_ships = {'Battleship': [], 'Cruiser': [], 'Destroyer': []}
     for ship in player_ships:
+        print_grid(player_grid)
         while True:
             try:
                 print(f"Place your {ship}...")
@@ -199,11 +240,16 @@ def computer_turn(grid_size, computer_shots_grid):
         x = random.randint(0, grid_size - 1)
         y = random.randint(0, grid_size - 1)
         if computer_shots_grid[y][x] == EMPTY:
+            print(f"Computer shoots at {chr(65 + x)} {y}")
             return x, y
 
 
 def main():
     print("Welcome to Battleships!")
+
+    # Instructions on how to play the game
+    print(INSTRUCTIONS)
+    print_example_grid()
 
     # Prompt the user to set the grid size
     grid_size = set_grid_size()
